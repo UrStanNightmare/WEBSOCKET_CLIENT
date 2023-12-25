48933c140f1c76efd33a9e5ca90553b32b5e3dc4
Anton Oshchepkov <antonoschepkov27@gmail.com>
2023-12-26 03:52:45 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import {Stomp} from "@stomp/stompjs";
+import * as SockJS from "sockjs-client"
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state,setState] = useState({});
+
+    let stompClient
+
+    const currentUser = {
+        id: 12412152512,
+        name: "vasya",
+    }
+    const connect = () => {
+        console.log("Connect()")
+        SockJS = new SockJS("http://localhost:8080/ws");
+
+        stompClient = Stomp.over(SockJS);
+        stompClient.connect({}, onConnected, onError);
+    };
+
+    const onMessageReceived = (message) => {
+        console.log("Message recieved" + message)
+        const result = JSON.parse(message.body)
+        setState(result)
+    }
+    const onConnected = () => {
+        console.log("connected");
+
+        stompClient.subscribe(
+            "/user/" + currentUser.id + "/queue/messages",
+            onMessageReceived
+        );
+    };
+
+    const onError = (e) => {
+        console.log("Error!")
+        console.log(e)
+    }
+
+    function sendFunc() {
+        const message = {
+            senderId: currentUser.id,
+            chatId: 1245125512,
+            content: "dolboeb",
+        };
+        stompClient.send("/app/chat", {}, JSON.stringify(message));
+    }
+
+    return (
+        <div className="App">
+            <button onClick={connect}>
+                CONNECT
+            </button>
+            <button onClick={sendFunc}>
+                SEND
+            </button>
+
+            <pre>
+                {JSON.stringify(state)}
+            </pre>
+        </div>
+    );
 }
 
 export default App;
